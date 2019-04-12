9bbe45d9df1912c8f951845cee63955a59e3124a
alex <alex@alex.local>
2019-04-12 23:44:35 +0900

    검색하니 이미 구현된게 있네..

---
+++ src/core/index.js
@@ -0,0 +1,5 @@
+import atoh from './atoh.js'
+
+export function getConvertText(text) {
+  return AtoH.convert(text);
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="home-view">
+    <textarea class="input-text" @keyup="setKeyUp" />
+    <div class="out-text" />
   </div>
 </template>
 
 <script>
+import { getConvertText } from '@/core'
+
 export default {
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+  methods: {
+    setKeyUp(e) {
+      $('.out-text').html( getConvertText( $('.input-text').val() ) );
+    },
   }
 }
 </script>
@@ -16,6 +20,22 @@ export default {
 <style lang="scss">
 
 #home-view {
+  height: 100%;
+  padding: 60px;
+  .input-text {
+    width: 100%;
+    height: calc( 50vh - 30px );
+    margin-bottom: 30px;
+    padding: 0;
+    resize: none;
+  }
+  .out-text {
+    width: 100%;
+    height: calc( 50vh - 30px );
+    margin-top: 30px;
+    border: 1px solid black;
+  }
+
 }
 
 </style>
